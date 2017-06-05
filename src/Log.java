5b0040514a8847fec5a4768873274d6104a862a9
365460 <365460@gmail.com>
2017-06-05 15:30:00 +0800

    add gitignore

---
+++ src/Log.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Rober on 2017/6/4.
+ */
+public class Log {
+}
---
+++ src/Reminder/Broadcast.java
@@ -0,0 +1,7 @@
+package Reminder;
+
+/**
+ * Created by Rober on 2017/6/4.
+ */
+public class Broadcast {
+}
